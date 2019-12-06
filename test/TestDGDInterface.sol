5bd47895a9962f4afaaa128b99c6d7f3964f332b
Anthony Eufemio <ace@digix.global>
2019-12-06 13:58:56 -0800

    updated tests

---
+++ test/TestDGDInterface.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.25 <0.6.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/DGDInterface.sol";
+
+contract TestDGDInterface {
+
+  function testInitialBalanceUsingDeployedContract() public {
+    DGDInterface dgd = DGDInterface(DeployedAddresses.DGDInterface());
+
+    uint expected = 2000000000000000;
+
+    Assert.equal(dgd.balanceOf(tx.origin), expected, "Owner should have 2000000000000000 DGD initially");
+  }
+
+  function testInitialBalanceWithNewDGDInterface() public {
+    DGDInterface dgd = new DGDInterface();
+
+    uint expected = 2000000000000000;
+
+    Assert.equal(dgd.balanceOf(tx.origin), expected, "Owner should have 2000000000000000 MetaCoin initially");
+  }
+
+}
