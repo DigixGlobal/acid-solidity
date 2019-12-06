973c90a5013a23806578ef14421280e4360330ff
Anthony Eufemio <ace@digix.global>
2019-12-05 20:16:53 -0800

    update for Solidity 0.5.x

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const DGDInterface = artifacts.require("DGDInterface");
+const Acid = artifacts.require("Acid");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(DGDInterface);
+  deployer.deploy(Acid);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,75 @@
+const LightWalletProvider = require('@digix/truffle-lightwallet-provider');
+
+const { KEYSTORE, PASSWORD } = process.env;
+
+if (!KEYSTORE || !PASSWORD) { throw new Error('You must export KEYSTORE and PASSWORD (see truffle.js)'); }
+
 module.exports = {
-  // Uncommenting the defaults below 
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
-  //
+  networks: {
+    kovan: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://kovan.infura.io/',
+        pollingInterval: 2000,
+        // debug: true,
+      }),
+      gas: 6850000,
+      gasPrice: 20 * (10 ** 9),
+      network_id: '42',
+    },
+    rinkeby: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://rinkeby.infura.io/',
+        pollingInterval: 2000,
+        // debug: true,
+      }),
+      gas: 6850000,
+      gasPrice: 20 * 10 ** 9,
+      network_id: '4',
+    },
+    mainnet: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://mainnet.digix.global/',
+        pollingInterval: 15000,
+        // debug: true,
+      }),
+      gas: 7900000,
+      gasPrice: 40 * (10 ** 9),
+      network_id: '1',
+    },
+    classic: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://digixparity04.digix.io/',
+        pollingInterval: 5000,
+        // debug: true,
+      }),
+      gas: 400000,
+      network_id: '61',
+    },
+    development: {
+      host: process.env.DOCKER ? 'ganache' : 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 7850000,
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: false,
+    },
+  },
+  mocha: process.env.REPORT_GAS ? {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 21,
+    },
+  } : {},
 };
